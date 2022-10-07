e191edbe25e500cc75b028b1c2f5acb1bd9a5402
Raisal <raisal12312@mail.ru>
2022-10-07 04:09:29 +0300

    add Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+
+// Задача 1.
+
+Console.Write("input a first number: ");
+int n1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("input a second number: ");
+int n2 = Convert.ToInt32 (Console.ReadLine());
+
+int quad2 = n2 * n2;
+
+if (n1 == quad2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+/*
+
+// Задача 2.
+
+Console.Write("input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = -number;
+
+int current;;
+if(number > 0)
+
+
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+
+*/
+
+//Задача 3.
+
+/*
+Console.Write("inut three-digit number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n >= 100 && n < 1000)
+{
+    int lastDigit = n % 10;
+    Console.WriteLine($"Last digit of {n} is {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Uncurrent input");
+}
+
+*/
